a186e9f4c5b2a2d95a2b8ee4c812abbf1068e670
DimaSkid <dimitri.bacot@etu.univ-st-etienne.fr>
2025-11-07 15:34:34 +0100

    ajout de test

---
+++ SymfonyWeb/src/Controller/ControllerExempleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ControllerExempleController extends AbstractController
+{
+    #[Route('/controller/exemple', name: 'app_controller_exemple')]
+    public function index(): Response
+    {
+        return $this->render('controller_exemple/index.html.twig', [
+            'controller_name' => 'ControllerExempleController',
+        ]);
+    }
+}
